0a84721cd7a4b2a59e65abda17aea2b41bae80df
Juliodominguezcalderon <ing.juliodominguez@gmail.com>
2024-11-03 22:50:16 -0500

    adicionar graficas

--- main.py
+++ main.py
@@ -7,15 +7,23 @@ inicio = "2004-1-1" # Definimos la fecha de inicio para analisis de las acciones
 fin = "2023-12-31"  # Definimos la fecha final del analsis de las acciones
 
 #Traemos los datos extraídos
-historico_acciones = proyecto.extracción.extraer_datos(acciones, inicio, fin)
+consolidado_acciones = proyecto.extracción.extraer_datos(acciones, inicio, fin)
+consolidado_acciones["Año"] = pd.DatetimeIndex(consolidado_acciones["Date"]).year
 
+#print(consolidado_acciones.head())
 # Vista rápida de datos:
-print("Información general de las columnas: \n")
-print(historico_acciones.info(), "\n")
-print("Resumen estadístico de las columnas: \n")
-print(historico_acciones.describe())
+#print("Información general de las columnas: \n")
+#print(consolidado_acciones.info(), "\n")
+#print("Resumen estadístico de las columnas: \n")
+#print(consolidado_acciones.describe())
 
 #Visualizamos los datos
-proyecto.visualización.graficar_historico(historico_acciones)
+proyecto.visualización.graficar_historico(consolidado_acciones)
 
-proyecto.visualización.graficar_histograma(historico_acciones)
+proyecto.visualización.graficar_histograma(consolidado_acciones)
+
+#proyecto.visualización.graficar_barras(consolidado_acciones)
+
+proyecto.visualización.graficar_dispersion(consolidado_acciones)
+
+proyecto.visualización.graficar_historico2(consolidado_acciones)
--- proyecto/visualización.py
+++ proyecto/visualización.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
+import plotly.express as px
 
 acciones = ["AAPL","MSFT","GOOG","NVDA"] #Definimos las acciones objeto de analisis
 
-def graficar_historico(historico_acciones):  
+def graficar_historico(consolidado_acciones):  
     for accion in acciones:
-        datos_accion = historico_acciones[historico_acciones['Ticker'] == accion]
+        datos_accion = consolidado_acciones[consolidado_acciones['Ticker'] == accion]
         plt.plot(datos_accion['Date'], datos_accion['Close'], label=accion)   
     plt.xlabel('Fecha')
     plt.ylabel('Valor Cierre')
@@ -13,9 +14,47 @@ def graficar_historico(historico_acciones):
     plt.grid(True)
     plt.show()
 
-def graficar_histograma(historico_acciones):
-    plt.hist(historico_acciones["Close"], bins=30, alpha=0.75, color="blue", edgecolor="black")
+def graficar_histograma(consolidado_acciones):
+    for accion in acciones:
+        datos_accion = consolidado_acciones[consolidado_acciones['Ticker'] == accion]
+        plt.hist(datos_accion["Close"], label=accion, bins=10, alpha=0.75, edgecolor="black")
     plt.title("Histograma de precio de cierre de las acciones")
     plt.xlabel("Precio cierre")
     plt.ylabel("Frecuencia")
     plt.show()
+
+def graficar_barras(consolidado_acciones):
+    for accion in acciones:
+        datos_accion = consolidado_acciones[consolidado_acciones['Ticker'] == accion]
+        plt.bar(datos_accion["Ticker"], datos_accion["Close"])
+    plt.title("Gráfico de barras de los precios de cierre de las acciones")
+    plt.xlabel("")
+    plt.ylabel("")
+    plt.show()
+
+def graficar_dispersion(consolidado_acciones):
+    for accion in acciones:
+        datos_accion = consolidado_acciones[consolidado_acciones['Ticker'] == accion]
+        plt.scatter(datos_accion["Open"], datos_accion["Close"], marker="o") # Se tomaron dos variables cuantitativas
+    plt.title("Gráfico de Dispersión")
+    plt.xlabel("Precio Apertura")
+    plt.ylabel("Precio Cierre")
+    plt.show()
+
+def graficar_historico(consolidado_acciones):  
+    for accion in acciones:
+        datos_accion = consolidado_acciones[consolidado_acciones['Ticker'] == accion]
+        plt.plot(datos_accion['Date'], datos_accion['Close'], label=accion)   
+    plt.xlabel('Fecha')
+    plt.ylabel('Valor Cierre')
+    plt.title('Valor de cierre de las Acciones')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+def graficar_historico2(consolidado_acciones):  
+
+    fig = px.line(consolidado_acciones, x="Date", y="Close", color="Ticker", title='Precio de Cierre de Acciones')
+    fig.update_xaxes(title_text='Fecha')
+    fig.update_yaxes(title_text='Precio de Cierre')
+    fig.show()
