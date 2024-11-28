7596cfb32781ce7d81783f1637b4471c086ec7e6
Juliodominguezcalderon <52422412+Juliodominguezcalderon@users.noreply.github.com>
2024-11-27 21:14:20 -0500

    Delete proyecto directory

--- proyecto/__init__.py
+++
@@ -1,3 +0,0 @@
-# __init__.py
-import proyecto.descargar as descargar
-import proyecto.visualización as visualización
--- proyecto/carga.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def cargar_datos(consolidado_acciones):
-    consolidado_acciones = pd.read_csv("consolidado_acciones.csv", delimiter=",")
-    consolidado_acciones.columns.values[0] = 'Indice' #Nombramos la primer columna con el nombre de Indice
-    consolidado_acciones['Date'] =pd.to_datetime(consolidado_acciones['Date']) #Aseguramos que la columna Date sea en formato fecha 
-    return(consolidado_acciones) 
-
